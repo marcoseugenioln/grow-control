1501df7a2eebb81c2c3b0075e58fd44c1b8480ae
Marcos Eugênio <marcos.eugenioln@gmail.com>
2022-12-07 09:03:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// Importing express module
+const express = require("express")
+const app = express()
+  
+// Handling GET / request
+app.use(express.static(__dirname + '/')) 
+  
+// Handling GET /hello request
+app.get("/hello", (req, res, next) => {
+    res.send("This is the hello response");
+})
+
+// Server setup
+app.listen(3000, () => {
+    console.log("Server is Running")
+})
---
+++ view/scripts/login.js
@@ -0,0 +1,11 @@
+function validate() {
+    
+    var user = document.getElementById('name').textContent;
+    var pass = document.getElementById('pass').textContent;
+
+    if (user == 'marcos' && pass == '#Dv010400') {
+        
+        alert('Login valido')
+    }
+
+}
