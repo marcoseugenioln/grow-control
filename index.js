3a48505a71704eff8303c260e3911f79cb94b9f6
Marcos Eugênio <marcos.eugenioln@gmail.com>
2022-12-07 09:38:56 -0300

    Update index.js
    change port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/hello", (req, res, next) => {
 })
 
 // Server setup
-var PORT = 80
+var PORT = 443
 app.listen(PORT, () => {
     console.log("Server is Running. Listening to port " + PORT)
 })
