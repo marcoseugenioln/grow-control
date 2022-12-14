d72baf9ac4bf2ac0873a396a42c573404aecf1a7
Marcos Eugênio <marcos.eugenioln@gmail.com>
2022-12-13 22:26:47 -0300

    final commit v0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 // Importing express module
+const { response } = require("express");
 const express = require("express")
 const sqlite3 = require("sqlite3");
 const bodyParser = require("url");
@@ -11,6 +12,11 @@ app.use(express.static(__dirname + '/'));
 // CONSTANTS
 const DATABASE = "database.db"
 var db;
+
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
 
 // Server setup
 var PORT = 3000
@@ -27,35 +33,45 @@ app.get("/hello", (req, res, next) => {
 // Handling GET /insert request
 app.get("/insert", (req, res, next) => {
 
+    var date = new Date();
+
+    date.addHours(-3);
+
     var su_1 = req.query.su_1;
     var su_2 = req.query.su_2;
     var su_3 = req.query.su_3;
     var su_4 = req.query.su_4;
     var au    = req.query.au;
     var t    = req.query.t;
-    var response = 
-    `su_1=${su_1} 
-    su_2=${su_2} 
-    su_3=${su_3} 
-    su_4=${su_4} 
-    su_4=${su_4} 
-    au=${au} 
-    t=${t}`
 
-    console.log(response);
-    
-    res.send(response);
+    var post = `su_1=${su_1} su_2=${su_2} su_3=${su_3} su_4=${su_4} su_4=${su_4} au=${au} t=${t}`
+
+    console.log(post);
 
     var db = new sqlite3.Database(DATABASE);
+    
+    var current_date = date.toISOString().split('T')[0] + ' ' + date.toISOString().split('T')[1].split('.')[0];
+    console.log(current_date);
 
-    db.run(
-        `INSERT INTO Grow(
-            su_1, su_2, su_3, su_4, au, t, date
-        ) 
-        VALUES (
-            ${su_1}, ${su_2}, ${su_3}, ${su_4}, ${au}, ${t}, ${date}
-        )`
-    )
+    var query = 
+
+    `INSERT INTO Grow(
+        soil_umidity_1, soil_umidity_2, soil_umidity_3, soil_umidity_4, air_umidity, temperature, date
+    ) 
+    VALUES (
+        ${su_1}, ${su_2}, ${su_3}, ${su_4}, ${au}, ${t}, '${current_date}'
+    )`;
+
+    console.log(query)
+    db.run(query)
+
+    if (su_1 < 40) {
+        res.send('1');
+    }
+    else{
+        res.send('0');
+    }
+    
 })
 
 // Handling GET /run-query request
