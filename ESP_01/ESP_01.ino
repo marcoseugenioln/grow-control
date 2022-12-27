3230f897298b7a00f73c42c794931f6b0016c584
Marcos Eugênio <marcos.eugenioln@gmail.com>
2022-12-27 10:04:23 -0300

    arduino
    update arduino

--- ESP_01/ESP_01.ino
+++ ESP_01/ESP_01.ino
@@ -1,9 +1,13 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <DHT.h>
 
-const int pin_sensor  = 34; 
+const int pin_su_sensor  = 34; 
 const int pin_led_net = 32;
-const int pin_led_req = 25;
+const int pin_led_water = 35;
+const int pin_led_req = 34;
+const int pin_dht     = 2;
+const int pin_bomb    = 13;
 
 const String server_name   = "http://192.168.1.66:3000/insert";
 
@@ -21,18 +25,21 @@ const char* password = "#Dv010400";
 int delay_count = 0;
 int count = 0;
 
+DHT dht(pin_dht, DHT11);
+
 void setup(){
   
-  Serial.begin(9600); //INICIALIZA A SERIAL
+  Serial.begin(9600);
 
   pinMode(pin_led_req, OUTPUT);
   pinMode(pin_led_net, OUTPUT);
+  dht.begin();
 
-  WiFi.begin(ssid, password);             // Connect to the network
+  WiFi.begin(ssid, password);             
   Serial.print("Connecting to ");
   Serial.print(ssid);
 
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  while (WiFi.status() != WL_CONNECTED) { 
     delay(500);
     digitalWrite(pin_led_net, HIGH);    
     Serial.print('.');
@@ -45,10 +52,10 @@ void setup(){
   Serial.println(WiFi.localIP());
   digitalWrite(pin_led_net, HIGH);   
 
-  //pinMode(pin_bomb, OUTPUT);
+  pinMode(pin_bomb, OUTPUT);
   
-  Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
-  delay(2000); //INTERVALO DE 2 SEGUNDOS
+  Serial.println("Lendo a umidade do solo...");
+  delay(2000); 
 }
 
 void loop(){  
@@ -60,27 +67,37 @@ void loop(){
     digitalWrite(pin_led_net, HIGH);    
   }
        
-  
-  read_value = constrain(analogRead(pin_sensor),a_read_wet,a_read_dry); //MANTÉM read_value DENTRO DO INTERVALO (ENTRE a_read_wet E a_read_dry)
+  digitalWrite(pin_bomb, HIGH);
+  read_value = constrain(analogRead(pin_su_sensor),a_read_wet,a_read_dry); //MANTÉM read_value DENTRO DO INTERVALO (ENTRE a_read_wet E a_read_dry)
   read_value = map(read_value,a_read_wet,a_read_dry,max_u,min_u); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
   
-
+  Serial.write(int(read_value));
+  Serial.write("\n");
+  
   delay(1000); 
   delay_count+=1000;
+  
   
   count += read_value;
 
   if(int(delay_count) >= 60000){
+    
 
     
 
     int su_1 = count / 60;
+    float au = dht.readHumidity();
+    float t = dht.readTemperature();
     
-    Serial.print("su1: "); 
+    Serial.print("Soil Umidity: "); 
     Serial.print(su_1);
-    Serial.print("%\n");
-        
-    String url = server_name + "?su_1=" + String(su_1) + "&su_2=0&su_3=0&su_4=0&au=0&t=0";
+    Serial.print(" Air Humidity: ");
+    Serial.println(au);    
+    Serial.print(" Temperature: ");
+    Serial.print(t);
+    Serial.print("ºC\n");
+    
+    String url = server_name + "?su_1=" + String(su_1) + "&su_2=0&su_3=0&su_4=0&au=" + String(au) + "&t=" + String(t);
     Serial.print(url);
     
     http.begin(url.c_str());
